5705ddab8a9d100da4540a6d9f924dac304e02ed
Jean-Philippe <68045559+OpaiChinaCat@users.noreply.github.com>
2020-07-09 17:42:03 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-    if (x >= 0) {
-        led.unplot(x, y)
-        x += -1
+    if (x < 0) {
+        if (x < 0) {
+            led.unplot(x, y)
+            x += -1
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+            y += -1
+            x = 0
+            led.plot(x, y)
+        }
+    } else {
+        x = 0
+        y = 0
         led.plot(x, y)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (y <= 4) {
+        if (x < 4) {
+            led.unplot(x, y)
+            x += 1
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+            y += 1
+            x = 0
+            led.plot(x, y)
+        }
     } else {
         x = 4
+        y = 4
         led.plot(x, y)
     }
 })
 let x = 0
 let y = 0
 y = 0
-x = 4
+x = 2
 led.plot(x, y)
 basic.forever(function () {
 	
